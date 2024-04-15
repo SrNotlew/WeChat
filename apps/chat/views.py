c80374f46ee86f0664ca3c6c566ab979ef780562
Notlew <rwelton284@gmail.com>
2024-04-14 21:37:33 -0300

    Primeiro commit do app WeChat

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
-def index(req):
-    return HttpResponse("My First Page")
+def home(request):
+    return render(request,"index.html", {} )
